740f5b77ae0c2a30854a3eb9974367f03911f849
CP <Sharath.CP@buck.com>
2024-02-19 21:08:33 +0530

    Spring Security JWT

--- src/main/java/com/gateway/api/config/SecurityConfig.java
+++ src/main/java/com/gateway/api/config/SecurityConfig.java
@@ -1,31 +1,45 @@
 package com.gateway.api.config;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.gateway.api.service.CustomUserDetailsService;
 
-
-
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfiguration{
-	
-	@Autowired
-	private CustomUserDetailsService customUserDetailsService;
-	
-	public void configure(HttpSecurity http) throws Exception {
-		http.userDetailsService(customUserDetailsService);
-	}
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
-		
-	}
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder http) throws Exception {
+        http.userDetailsService(customUserDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    @Bean(name=BeanIds.AUTHENTICATION_MANAGER)
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests().antMatchers("/authentication").permitAll().anyRequest().authenticated();
+    }
+    
 }
--- src/main/java/com/gateway/api/controller/WelcomeController.java
+++ src/main/java/com/gateway/api/controller/WelcomeController.java
@@ -1,13 +1,40 @@
 package com.gateway.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.gateway.api.entity.AuthRequest;
+import com.gateway.api.util.JwtUtil;
 
 @RestController
 public class WelcomeController {
-	
+	@Autowired
+	private JwtUtil jwtUtil;
+
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
 	@GetMapping("/")
 	public String welcome() {
 		return "welcome";
 	}
+
+	@PostMapping("/authentication")
+	public String generateToken(@RequestBody AuthRequest authRequest) throws Exception {
+		try {
+			authenticationManager.authenticate(
+					new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+		} catch (Exception e) {
+			throw new Exception("Invalid username and password");
+		}
+		return jwtUtil.generateToken(authRequest.getUsername());
+	}
+	
+	
 }
